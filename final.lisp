5a2ace71ee6f50797f6febc477f77fa3fccb432c
barisayyildiz <barisayyildiz.1799@gmail.com>
2022-01-17 18:59:20 +0300

    update

--- final.lisp
+++ final.lisp
@@ -1,47 +1,67 @@
+(defun lastelement(l)
+	(cond
+		((= (length l) 1) (car l))
+		(t (lastelement (cdr l)))
+	)
+)
+;; (write (lastelement '(4 5 6 7)))
+
+(defun removelast(l)
+	(cond
+		((= (length l) 1) ())
+		(t (cons (car l) (removelast (cdr l))))
+	)
+)
+;; (write (removelast '(4 5 6 7)))
+
+(defun ismember(l x)
+	(cond 
+		((zerop (length l)) Nil)
+		((= (car l) x) T)
+		(t (ismember (cdr l) x))
+	)
+)
+;; (write (ismember '(1 2 3) 3))
 
 
-;; ;; return l[p] == e
-;; (defun findit(l e p)
-;; 	(cond
-;; 		((< (length l) p) Nil)
+;This function adds an element in the nth position of a list
+(defun add-nth (l e p) 
+	(cond 
+		((< (length l ) p) Nil) 
+		((= p 1 ) (cons e l)) 
+		(t (cons (car l) (add-nth (cdr l) e ( - p 1))))
+	)
+)
 
-;; 		((= p 1) (eq (car l) e))
-
-;; 		(t
-;; 			(findit (cdr l) e (- p 1))
-;; 		)
-
-;; 	)
-;; )
+;; return l[p] == e
+(defun findit(l e p)
+	(cond
+		((< (length l) p) Nil)
+		((= p 1) (eq (car l) e))
+		(t (findit (cdr l) e (- p 1)))
+	)
+)
 ;; (write (findit '(5 4 3 2 7 1 8) 7 5))
 
 
-;; (defun minimum(l)
-;; 	;; length is 1
-;; 	(if (= (length l) 1)
-;; 		(car l)
-;; 		(if (< (car l) (minimum (cdr l)))
-;; 			(car l)
-;; 			(minimum (cdr l))
-;; 		)
-;; 	)
-;; )
+(defun minimum(l)
+	;; length is 1
+	(if (= (length l) 1)
+		(car l)
+		(if (< (car l) (minimum (cdr l)))
+			(car l)
+			(minimum (cdr l))
+		)
+	)
+)
 ;; (write (minimum '(5 4 3 2 7 1 8)))
 
-;; (defun minimum(l)
-;;     (cond ((null (cdr l)) (car l))
-;;         ( (< (car l) (minimum (cdr l))) (car l))
-;;         (t
-;;             (minimum 
-;;                 (cdr l)))))
-;; (write (minimum '(1 2 3 -3 12 )))
-
-;; ;; fibonacci
-;; (defun fibo(n)
-;; 	(if (or (zerop n) (= n 1))
-;; 		1
-;; 		(+ (fibo (- n 1)) (fibo (- n 2)))
-;; 	)
-;; )
+;; fibonacci
+(defun fibo(n)
+	(if (or (zerop n) (= n 1))
+		1
+		(+ (fibo (- n 1)) (fibo (- n 2)))
+	)
+)
 ;; (write (fibo 10))
 
