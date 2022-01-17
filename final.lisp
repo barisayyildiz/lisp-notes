d0beeba0aaccee67776d2504e35e9b0c72d32a2c
barisayyildiz <barisayyildiz.1799@gmail.com>
2022-01-17 17:35:14 +0300

    update

--- final.lisp
+++ final.lisp
@@ -1,4 +1,22 @@
+;; (defun minimum(l)
+;; 	;; length is 1
+;; 	(if (= (length l) 1)
+;; 		(car l)
+;; 		(if (< (car l) (minimum (cdr l)))
+;; 			(car l)
+;; 			(minimum (cdr l))
+;; 		)
+;; 	)
+;; )
+;; (write (minimum '(5 4 3 2 7 1 8)))
 
+;; (defun minimum(l)
+;;     (cond ((null (cdr l)) (car l))
+;;         ( (< (car l) (minimum (cdr l))) (car l))
+;;         (t
+;;             (minimum 
+;;                 (cdr l)))))
+;; (write (minimum '(1 2 3 -3 12 )))
 
 ;; ;; fibonacci
 ;; (defun fibo(n)
